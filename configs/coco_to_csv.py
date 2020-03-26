8ad2ca616b1059c2baa3b871c0896972c5d55f0d
maxfashko <maxfashko@gmail.com>
2020-03-26 16:53:34 +0700

    added train/test split for csv dataset

--- configs/coco_to_csv.py
+++ configs/coco_to_csv.py
@@ -1,23 +1,16 @@
 data = dict(
     input_data=dict(
         type="CocoDataset",
-        data_root='/home/maksim/data/vizorlabs/DATASETS/',
-        dir_name='persons_all_flow',
+        data_root='/DATASETS/',
+        dir_name='coco/',
         images='images/',
-        annotations='annotation_data.json',
+        annotations='annotations/annotation_data.json',
     ),
 
     output_data=dict(
         type="CsvDataset",
         data_root='/tmp/',
         dir_name='csv',
-        annotations='annotation_data.csv',
-        params=dict(
-            train_test_split=dict(
-                train=0.8,
-                test=0.2,
-                seed=42
-            )
-        )
+        annotations='annotation_data.csv'
     )
 )
